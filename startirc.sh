d1f77bccb67c94aba6d3f1da6e7d073640300cba
mempomisc <mempo.misc@onet.pl>
2014-06-01 23:55:38 +0200

    [merge] happuri version of startirc.sh

--- startirc.sh
+++ startirc.sh
@@ -1,39 +1,8 @@
 #!/bin/bash
 
-light_red='\e[1;31m' 
-NC='\e[0m' # No Color
-
-irc_conf_file="irc.conf"
-
-# Config file exist?
-if [[ ! -r "$irc_conf_file" ]] ; then  
-	echo -e "${light_red}Can not find conf_file $conf_file ${NC}"
-	exit 1
-
-fi 
-
-
-all_conf=$(cat $irc_conf_file | sed '/^$/d' | sed  '/^#/ d')
-
-source $irc_conf_file
-
-#BASEDIR=$(echo "$all_conf" |  awk '{ print $1 }')
-#HOST=$(echo "$all_conf" |  awk '{ print $2 }')  
-#PORT=$(echo "$all_conf" |  awk '{ print $3 }')
-
-# ii package installed?
-if ! which ii >/dev/null  ; then
-    echo -e "${light_red}Package ii must be installed!!${NC}"
-	exit 2
-fi
-
-
-
-killall ii &> /dev/null
-
-set -x
-nohup ii -f "$HOSTNAME" -n mempobot -i "$BASEDIR/" -s "$HOST" -p "$PORT" &
-set +x
+BASEDIR="./var/link"
+HOST="127.0.0.1"
+nohup bash ii_forever.sh -f "$HOSTNAME" -n mempobot -i "$BASEDIR/" -s "$HOST" -p 6668 &
 
 echo "sleep"
 sleep 15
@@ -42,9 +11,8 @@ echo "/JOIN #mempo" > "$BASEDIR/$HOST/in"
 
 echo "done"
 sleep 2
-#echo "Showing out"
-#cat "$BASEDIR/$HOST/out"
+echo "Showing out"
+cat "$BASEDIR/$HOST/out"
 
-echo "All done" 
-exit 0
+echo "All done"
 
