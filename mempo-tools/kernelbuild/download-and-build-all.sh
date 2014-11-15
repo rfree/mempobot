b7d8e19830fa920473b09a1ab1fd6ab12f934bef
mempomisc <mempo.misc@onet.pl>
2014-11-15 18:18:14 +0100

    using new flavour names like deb7/deskmax
    also gitignore

--- mempo-tools/kernelbuild/download-and-build-all.sh
+++ mempo-tools/kernelbuild/download-and-build-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./download-and-build.sh "desk"
-./download-and-build.sh "deskmax"
+./download-and-build.sh "deb7/desk"
+./download-and-build.sh "deb7/deskmax"
 
 
--- mempo-tools/kernelbuild/download-and-build.sh
+++ mempo-tools/kernelbuild/download-and-build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+echo
+echo "Going to start the Mempo secure kernel build. In case of trouble read all on http://mempo.org (or .i2p for i2p anonymous network) and ask us on IRC (wait for reply!)"
+echo 
+
 flavour=$1
 git_user=$2
 
-if [[ -z "$flavour" ]] ; then echo "Specify the flavour to build, like desk or deskmax etc"; fi
+cmd="git-gpg-check"
+if [[ ! -x $(which $cmd) ]] ; then { echo "No command ($cmd) is not found/not executable. You need to install this program and add to PATH. See the readme."  ; exit 1 ; } fi
+
+thedir="$HOME/deterministic-kernel"
+
+if [[ -z "$flavour" ]] ; then echo "Specify the flavour to build (as command line argument nr 1 to this script), like desk or deskmax, or use the word 'none' to just download/prepare"; exit 1 ; fi
 if [[ -z "$git_user" ]] ; then git_user="mempo" ; fi
 
 echo "Will build kernel:"
@@ -11,9 +20,9 @@ echo "  flavour=$flavour"
 echo "  git_user=$git_user"
 
 cd ~
-rm -rf deterministic-kernel/
-git clone https://github.com/mempo/deterministic-kernel.git
-cd deterministic-kernel/
+rm -rf "$thedir" 
+git clone "https://github.com/$git_user/deterministic-kernel.git" || { echo "ERROR: Failed to git clone" ; exit 1 ; }
+cd "$thedir" || { echo "ERROR: Can not enter $thedir" ; exit 1 ; } 
 
 gpg_was_ok=nope
 git_tag=$( git describe --tags )
@@ -30,14 +39,23 @@ else
 	exit 51
 fi
 
-echo "Ok starting build script:"
-./run-flavour.sh "$flavour"
+if [[ $flavour == 'none' ]] ;
+then
+	ls "$thedir"
+	echo "Ok all done, not building anything as requested."
+	echo "Files are ready in: $thedir"
+else
 
-echo "Build process finished, will now publish"
-mkdir -p ~/publish/
+	echo "Ok starting build script:"
+	./run-flavour.sh "$flavour"
+
+	echo "Build process finished, will now publish"
+	mkdir -p ~/publish/
 
 # we are in deterministic-kernel/
-cp -v kernel-build/linux-mempo/*deb ~/publish/ && echo "Copied the files" 
+	cp -v kernel-build/linux-mempo/*deb ~/publish/ && echo "Copied the files" 
+
+fi
 
 echo
 echo
