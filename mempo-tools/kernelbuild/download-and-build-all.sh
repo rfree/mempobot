e48808ebb3658eefefe99ffbb8b7a87aa0670f4b
mempomisc <mempo.misc@onet.pl>
2014-12-11 03:56:36 +0100

    download-and-build-all.sh asks for the git user

--- mempo-tools/kernelbuild/download-and-build-all.sh
+++ mempo-tools/kernelbuild/download-and-build-all.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-./download-and-build.sh "deb7/zero"
-./download-and-build.sh "deb7/zero"
-./download-and-build.sh "deb7/deskmax"
-./download-and-build.sh "deb7/desk"
-./download-and-build.sh "deb7/serv"
+echo
+echo "Going to start the Mempo secure kernel build. In case of trouble read all on http://mempo.org (or .i2p for i2p anonymous network) and ask us on IRC (wait for reply!)"
+echo "=== GOING to build ALL FLAVOURS ==="
+echo 
+
+git_user=$1
+
+cmd="git-gpg-check"
+if [[ ! -x $(which $cmd) ]] ; then { echo "No command ($cmd) is not found/not executable. You need to install this program and add to PATH. See the readme."  ; exit 1 ; } fi
+if [[ -z "$git_user" ]] ; then echo "Specify the flavour to build (as command line argument nr 1 to this script), like mempo or rfree (for github.com/rfree/)"; exit 1 ; fi
+
+echo "Building for user:"
+echo "git_user=$git_user"
+echo 
+
+./download-and-build.sh "deb7/maxserv" "$git_user"
+./download-and-build.sh "deb7/zero" "$git_user"
+./download-and-build.sh "deb7/zero" "$git_user"
+./download-and-build.sh "deb7/deskmax" "$git_user"
+./download-and-build.sh "deb7/desk" "$git_user"
+./download-and-build.sh "deb7/serv" "$git_user"
 
 
